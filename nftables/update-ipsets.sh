e826b184d44431d9c9c4c5d407950b6cd8754e9f
nucleo <nucleo@fedoraproject.org>
2019-08-15 23:19:20 +0300

    full path for binaries

--- nftables/update-ipsets.sh
+++ nftables/update-ipsets.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-curl -s https://iplists.firehol.org/files/firehol_level1.netset -o /tmp/firehol_level1.netset -z /tmp/firehol_level1.netset
-curl -s https://iplists.firehol.org/files/firehol_level2.netset -o /tmp/firehol_level2.netset -z /tmp/firehol_level2.netset
-curl -s https://iplists.firehol.org/files/firehol_level3.netset -o /tmp/firehol_level3.netset -z /tmp/firehol_level3.netset
+/usr/bin/curl -s https://iplists.firehol.org/files/firehol_level1.netset -o /tmp/firehol_level1.netset -z /tmp/firehol_level1.netset
+/usr/bin/curl -s https://iplists.firehol.org/files/firehol_level2.netset -o /tmp/firehol_level2.netset -z /tmp/firehol_level2.netset
+/usr/bin/curl -s https://iplists.firehol.org/files/firehol_level3.netset -o /tmp/firehol_level3.netset -z /tmp/firehol_level3.netset
 
 echo 'define firehol_level1 = {' > /tmp/firehol.include
 sed '/^$/d;/^#/d;s/$/,/' /tmp/firehol_level1.netset >> /tmp/firehol.include
@@ -16,4 +16,4 @@ echo 'define firehol_level3 = {' >> /tmp/firehol.include
 sed '/^$/d;/^#/d;s/$/,/' /tmp/firehol_level3.netset >> /tmp/firehol.include
 echo "}" >> /tmp/firehol.include
 
-nft -f /etc/nftables/firehol.nft
+/usr/sbin/nft -f /etc/nftables/firehol.nft
